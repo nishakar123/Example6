94872cc9c7944f9d8d2785319bc19841d8a8de9f
nishakar123 <nishakar123@gmail.com>
2016-10-10 16:27:05 +0530

    Initial Commit

---
+++ src/IfElse.java
@@ -0,0 +1,12 @@
+
+public class IfElse {
+
+	public static void main(String[] args) {
+		if(args.length>0)
+			System.out.println("Arguments");
+		else
+			System.out.println("No Arguments");
+
+	}
+
+}
